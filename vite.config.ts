16a02f36f474c45ace5f8c13ed11188da9318a13
Chetan Patel <gamerchetan323@gmail.com>
2026-05-24 00:08:22 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -45,7 +45,6 @@ export default defineConfig(({ mode }) => {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.API_KEY_2': JSON.stringify(env.GEMINI_API_KEY_2),
-      'process.env.OPENROUTER_API_KEY': JSON.stringify(env.OPENROUTER_API_KEY)
     },
     resolve: {
       alias: {
